189963fcb64311e4a7e49ccd70cf595225992ef3
刘鹏飞 <liupengfei@mindlinker.com>
2022-04-21 19:40:58 +0800

    init

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,70 @@
+module.exports = {
+	title: 'pfLiu',
+	head: [
+		['link', {
+			rel: 'icon',
+			href: '/favicon.ico'
+		}]
+	],
+	base: '/',
+	dest: 'public',
+	smoothScroll: true,
+	plugins: [
+		[
+			'@vuepress/blog',
+			{
+				directories: [{
+						id: 'post',
+						dirname: '_posts',
+						path: '/',
+						pagination: {
+							lengthPerPage: 10
+						},
+
+					}
+				],
+				frontmatters: [{
+					// Unique ID of current classification
+					id: 'tag',
+					// Decide that the frontmatter keys will be grouped under this classification
+					keys: ['tag'],
+					// Path of the `entry page` (or `list page`)
+					path: '/tag/',
+					// Layout of the `entry page`
+					layout: 'Tags',
+					// Layout of the `scope page`
+					scopeLayout: 'Tag'
+				}]
+			}
+		]
+	],
+	theme: '@vuepress/theme-blog',
+	locales: {
+		'/': {
+			lang: 'zh-CN'
+		}
+	},
+	themeConfig: {
+		displayAllHeaders: true,
+		nav: [{
+				text: 'Home',
+				link: '/'
+			},
+			{
+				text: 'Tags',
+				link: '/tag/'
+			},
+			{
+				text: 'Github',
+				link: 'https://github.com/lppppppf'
+			}
+		],
+		footer: {
+			contact: [{
+				type: 'github',
+				link: 'https://github.com/lppppppf'
+			}]
+
+		}
+	}
+}
---
+++ blog/_posts/2022-04-20-helloword.md
@@ -0,0 +1,11 @@
+---
+title: Hello World
+date: 2022-04-20
+tag: vue
+author: pfLiu
+location: Guangzhou  
+---
+
+> This is official blog theme.
+
+My content.
